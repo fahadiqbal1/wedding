179bf82a0007120bf0513e434bd6ec48cdf52737
Fahad Iqbal <iqbal.fahad90@gmail.com>
2017-05-05 23:25:33 +0100

    Adding forms

--- _pages/rsvp.md
+++ _pages/rsvp.md
@@ -6,4 +6,39 @@ sort: 4
 excerpt: Let us know if you can make the event, we would be honored to have you attend
 read_more: RSVP
 image: 1.jpg
+hide_contact_form: true
 ---
+
+<form method="post" action="#" id="rsvp">
+    <div class="field">
+        <label for="name">What is your name?</label>
+        <input type="text" name="name" id="name" required/>
+    </div>
+    <div class="field">
+        <label for="email">What is your email address?</label>
+        <input type="email" name="email" id="email" required />
+    </div>
+    <div class="field">
+        <label for="attending">Will you be joining us?</label>
+        <select name="attending" id="attending" required>
+            <option value="" selected></option>
+            <option value="Yes">Yes :D</option>
+            <option value="No">Sorry, I can't make it</option>
+        </select>
+    </div>
+    <div class="field attending-items">
+        <label for="party">How many people will be in your party?</label>
+        <input type="range" name="party" id="party" min="1" max="10" value="0" oninput="outputPartySize(value)">
+        <output for="party" id="partySize">Select a number</output>
+    </div>
+    <div class="field">
+        <label for="notes">Do you have any comments or any thing we should know about?</label>
+        <textarea name="notes" id="notes" rows="3" required></textarea>
+    </div>
+    <div class="field">
+        <div class="g-recaptcha" data-sitekey="6LdYLyAUAAAAAH1fcIccF8p4lj5AiajDOIrzghEW" data-callback="verifyReCaptchaRSVPForm"></div>
+    </div>
+    <ul class="actions">
+        <li><input type="submit" value="Send Response" /></li>
+    </ul>
+</form>
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,6 +1,82 @@
-//= require jquery.min
-//= require jquery.scrollex.min
-//= require jquery.scrolly.min
-//= require skel.min
-//= require util
-//= require massively
+(function($) {
+    $('form#contactUs .actions').hide();
+    $('form#rsvp .actions').hide();
+    $('form#rsvp .attending-items').hide();
+
+    var api = 'https://script.google.com/macros/s/AKfycbxOJ0QkVzoGK9wqyPw1x-ViXBhj29bOSCxXN7AroQn1zwyqIQ/exec';
+
+    $(document).on('submit','form#contactUs',function(e){
+        e.preventDefault();
+        var data = $("form#contactUs").serializeArray().reduce(function(m,o){ m[o.name] = o.value; return m;}, {})
+        $.ajax({
+            type: "POST",
+            url: api,
+            data: {
+                sheet_name: 'ContactForm',
+                Name: data.name,
+                Email: data.email,
+                Message: data.message,
+            },
+            success: function (response) {
+                $("form#contactUs").hide();
+                $("#contactForm").html("<strong>Thank you!</strong>");
+            }
+        });
+    });
+
+    $(document).on('submit','form#rsvp',function(e){
+        e.preventDefault();
+        var data = $("form#rsvp").serializeArray().reduce(function(m,o){ m[o.name] = o.value; return m;}, {})
+        $.ajax({
+            type: "POST",
+            url: api,
+            data: {
+                Name: data.name,
+                Email: data.email,
+                Attending: data.attending,
+                Notes: data.notes,
+                PartySize: data.party,
+            },
+            success: function (response) {
+                $("form#rsvp").hide();
+                alert("Thank you");
+            }
+        });
+    });
+
+    $(document).on('change', 'form#rsvp #attending', function(e){
+        var val = $(this).val();
+        if (val == 'Yes') {
+            $('form#rsvp .attending-items').show();
+        } else {
+            $('form#rsvp .attending-items').each(function(index, element){
+                $(element).find('*').filter(':input:visible:first').val('');
+            }).hide();
+        }
+    });
+
+})(jQuery);
+
+
+var verifyReCaptchaContactForm = function(){
+    $('form#contactUs .actions').show();
+    $('form#contactUs .g-recaptcha').hide();
+};
+var verifyReCaptchaRSVPForm = function(){
+    $('form#rsvp .actions').show();
+    $('form#rsvp .g-recaptcha').hide();
+};
+
+var outputPartySize = function(size){
+    document.querySelector('#partySize').value = size;
+};
+
+/* Diable enter submit */
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
